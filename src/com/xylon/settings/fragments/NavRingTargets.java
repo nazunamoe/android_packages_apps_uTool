b8a5d43ac2692ad395a2a39a0e64ff5ded92a45d
Daniel <daxx4182@gmail.com>
2013-05-07 00:28:52 +0800

    Just testing

--- src/com/xylon/settings/fragments/NavRingTargets.java
+++ src/com/xylon/settings/fragments/NavRingTargets.java
@@ -40,6 +40,7 @@ import android.preference.PreferenceFragment;
 import android.provider.MediaStore;
 import android.provider.Settings;
 import android.text.TextUtils;
+import android.util.ExtendedPropertiesUtils;
 import android.util.Log;
 import android.view.*;
 import android.widget.*;
@@ -132,14 +133,8 @@ public class NavRingTargets extends SettingsPreferenceFragment implements
 
         mPicker = new ShortcutPickerHelper(this, this);
         boolean tabletui = Settings.System.getInt(cr, Settings.System.CURRENT_UI_MODE, 0) == 1;
-        boolean dualpanel = Settings.System.getBoolean(cr, Settings.System.FORCE_DUAL_PANEL, false);
-        if (dualpanel) {
-            return inflater.inflate(tabletui ? R.layout.navigation_ring_targets_tablet_dual
+        return inflater.inflate(tabletui ? R.layout.navigation_ring_targets_tablet_dual
                     : R.layout.navigation_ring_targets, container, false);
-        } else {
-            return inflater.inflate(tabletui ? R.layout.navigation_ring_targets_tablet
-                    : R.layout.navigation_ring_targets, container, false);
-        }
     }
 
     @Override
