4bd2dd6f6473ddb8bcfc23476d1cd51f8bdf947e
Hieu Phan <aznrice2k4@gmail.com>
2013-06-14 23:27:36 -0500

    fixup! HALO update

--- src/com/xylon/settings/fragments/StatusExtras.java
+++ src/com/xylon/settings/fragments/StatusExtras.java
@@ -149,7 +149,7 @@ public class StatusExtras extends SettingsPreferenceFragment implements OnPrefer
                 Settings.System.HALO_REVERSED, 1) == 1);
 
         int isLowRAM = (ActivityManager.isLargeRAM()) ? 0 : 1;
-        mHaloPause = (CheckBoxPreference) prefSet.findPreference(KEY_HALO_PAUSE);
+        mHaloPause = (CheckBoxPreference) prefSet.findPreference(PREF_HALO_PAUSE);
         mHaloPause.setChecked(Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.HALO_PAUSE, isLowRAM) == 1);
 
